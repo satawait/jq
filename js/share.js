e6f658ad91f361f27f54eea94b76dc57aea5bf16
satawait <xingbangl@gmail.com>
2018-02-02 21:53:27 +0800

    no message

--- js/share.js
+++ js/share.js
@@ -45,6 +45,7 @@
   }
   function _initQQ(data) {
     var info = {title:data.title, desc:data.summary, share_url:data.url, image_url:data.pic};
+    console.log(info);
     function doQQShare() {
       try {
         console.log(data);
